ec3cda9220591cf20b90f1631622a09bafd8d707
Artyom Stetsenko <stetsenk@adobe.com>
2014-03-13 17:03:59 +0100

    Added first tool, HttpResourceMonitor. Sends an HTTP request over and over and logs changes in responses.
    Usage:
    
    HttpResourceMonitor [host [resource]]
    
    where
    * host is HTTP host (default: "localhost")
    * resource is path to resource on the host (default: "/")

---
+++ org.apache.sling.devops.tools/src/main/java/org/apache/sling/devops/tools/HttpResourceMonitor.java
@@ -0,0 +1,57 @@
+package org.apache.sling.devops.tools;
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.NoHttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpResourceMonitor {
+
+	private static final Logger logger = LoggerFactory.getLogger(HttpResourceMonitor.class);
+
+	public static void main(String[] args) throws IOException {
+		String host = args.length < 1 ? "localhost" : args[0];
+		String resource = args.length < 2 ? "/" : args[1];
+
+		try (CloseableHttpClient httpClient = HttpClients.custom().setRetryHandler(new DefaultHttpRequestRetryHandler(0, false)).build()) {
+
+			// Custom response handler
+			ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+				@Override
+				public String handleResponse(final HttpResponse response) throws IOException {
+					EntityUtils.consume(response.getEntity());
+					return response.getStatusLine().toString();
+				}
+			};
+
+			// Send requests
+			String prevResponse = null;
+			while (true) {
+				String curResponse;
+				try {
+					curResponse = httpClient.execute(
+							new HttpHost(host),
+							new HttpGet(resource),
+							responseHandler
+							);
+				} catch (NoHttpResponseException e) {
+					curResponse = "No response!";
+				} catch (IOException e) {
+					curResponse = "Connection aborted!";
+				}
+				if (prevResponse == null || !prevResponse.equals(curResponse)) {
+					logger.info(curResponse);
+				}
+				prevResponse = curResponse;
+			}
+		}
+	}
+}
