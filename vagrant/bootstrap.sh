46d4de926cf7916afd7427c4491089ff8cb25438
Artyom Stetsenko <stetsenk@adobe.com>
2014-05-16 16:47:46 +0200

    Updated Vagrant scenario to start instances with Crankstart instead of Launchpad standalone.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,20 +6,20 @@ apt-get install -y openjdk-7-jdk curl
 apt-get remove -y openjdk-6-jre-headless
 
 # Download and unpack Maven (because Ubuntu's package has too many unnecessary dependencies)
-if ! (ls apache-maven-3.2.1/bin/mvn &> /dev/null)
-then
-	rm -rf apache-maven*
-	wget http://mirror.switch.ch/mirror/apache/dist/maven/binaries/apache-maven-3.2.1-bin.tar.gz
-	tar -xzf apache-maven-3.2.1-bin.tar.gz
-fi
+#if ! (ls apache-maven-3.2.1/bin/mvn &> /dev/null)
+#then
+#	rm -rf apache-maven*
+#	wget http://mirror.switch.ch/mirror/apache/dist/maven/binaries/apache-maven-3.2.1-bin.tar.gz
+#	tar -xzf apache-maven-3.2.1-bin.tar.gz
+#fi
 
 # Build DevOps bundles
-apache-maven-3.2.1/bin/mvn clean install
+#apache-maven-3.2.1/bin/mvn clean install
 
 # Sling: kill running Sling, if any
-if [ -f sling.pid ]
+if [ -f crankstart-launcher.pid ]
 then
-	kill -9 $(cat sling.pid)
-	rm -f sling.pid
+	kill -9 $(cat crankstart-launcher.pid)
+	rm -f crankstart-launcher.pid
 fi
-rm -rf sling
+#rm -rf sling
