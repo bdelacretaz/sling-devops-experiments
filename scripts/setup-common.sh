e6d70a80758358d12924f72df76c9e0c2859eeba
Artyom Stetsenko <stetsenk@adobe.com>
2014-05-19 10:28:26 +0200

    Cleaned-up scripts unnecessary with Crankstart.

--- scripts/setup-common.sh
+++ scripts/setup-common.sh
@@ -10,29 +10,7 @@ dir_install="${dirs[0]}/install"
 # Sling
 login="admin:admin"
 
-# Bundles (as maven dependencies)
-bundles_common=(
-	"org.apache.zookeeper:zookeeper:3.3.6"
-	"org.apache.sling:org.apache.sling.devops.common:0.0.1-SNAPSHOT"
-	)
-
 ### Functions
-
-parse_and_check_maven_artifacts() {
-	local jars=()
-	for artifact in $@
-	do
-		local jar=$(parse_maven_artifact ${artifact})
-		if ! (ls ${jar} &> /dev/null)
-		then
-			echo "  ${jar} not found, please ensure all Maven artifacts are in your local repository, exiting" >& 2
-			echo "  (* hint: execute \"mvn dependency:get -Dartifact=${artifact}\")" >& 2
-			exit -1
-		fi
-		jars+=(${jar})
-	done
-	echo ${jars[@]} # return
-}
 
 copy_jars() {
 	local url=$1
@@ -42,14 +20,4 @@ copy_jars() {
 		echo "  ${jar}"
 		curl -sSu ${login} -T ${jar} "${url}"
 	done
-}
-
-parse_maven_artifact() {
-	local artifact=$1
-	local pieces=(${artifact//:/ })
-	local groupId=${pieces[0]}
-	local artifactId=${pieces[1]}
-	local version=${pieces[2]}
-	local jar=~/.m2/repository/${groupId//./\/}/${artifactId}/${version}/${artifactId}-${version}.jar
-	echo ${jar} # return
 }
--- scripts/setup-minion-repo.sh
+++ scripts/setup-minion-repo.sh
@@ -5,15 +5,6 @@
 # Common
 . $(dirname $0)/setup-common.sh
 
-# Bundles (as maven dependencies)
-bundles_minion=(
-	"org.apache.sling:org.apache.sling.hc.core:1.1.0"
-	"org.apache.sling:org.apache.sling.junit.core:1.0.8"
-	"org.apache.sling:org.apache.sling.junit.healthcheck:1.0.6"
-	"org.apache.sling:org.apache.sling.hc.webconsole:1.1.0" # useful
-	"org.apache.sling:org.apache.sling.devops.minion:0.0.1-SNAPSHOT"
-	)
-
 # Resources
 bundle_minion_C1="$(dirname $0)/../resources/org.apache.sling.samples.test-0.0.1.jar"
 bundle_minion_C2="$(dirname $0)/../resources/org.apache.sling.samples.test-0.0.2.jar"
@@ -46,13 +37,9 @@ sling=http://$2
 
 echo "-- Setting up repository for config ${config} via Sling instance at ${sling} --"
 
-# check if artifacts exist
-echo "Checking existence of artifacts..."
-jars=$(parse_and_check_maven_artifacts ${bundles_common[@]} ${bundles_minion[@]})
-
 # add config-determined bundle
 config_bundle_var=bundle_minion_${config}
-jars+=("${!config_bundle_var}")
+jars=("${!config_bundle_var}")
 
 # create paths
 echo "Creating repository paths..."
